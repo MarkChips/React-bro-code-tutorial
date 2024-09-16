5dfa8da2d5ff6dd58d7badd4a1ea7d32f40b90ce
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-16 17:48:38 +0100

    style: :lipstick: add Card styling, remove Food component import

--- my-react-app/src/Card.jsx
+++ my-react-app/src/Card.jsx
@@ -3,9 +3,9 @@ import profilePic from './assets/react-profile-pic.jpg';
 function Card() {
     return (
         <div className="card">
-            <img src={profilePic} alt="profile picture" />
-            <h2>Mark Chipperfield</h2>
-            <p>I am a Full-stack Software Developer and I am currently unemployed</p>
+            <img className="card-image" src={profilePic} alt="profile picture" />
+            <h2 className='card-title'>Mark Chipperfield</h2>
+            <p className='card-text'>I am a Full-stack Software Developer and I am currently looking for work</p>
         </div>
     );
 }
