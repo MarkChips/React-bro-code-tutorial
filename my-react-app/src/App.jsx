bc84ba8a61bc0d2b2279c34e30e03f9ca4acd155
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-17 17:57:53 +0100

    feat: :sparkles: add Button module with its own unique style

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Card from './Card.jsx';
+import Button from './Button/Button.jsx';
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
     <>
       <Header />
       <Card />
+      <Button />
       <Footer />
     </>
   );
