3f5bebb4ae489af7cba110595dc0a4d2e53fda90
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-18 16:35:20 +0100

    feat: :sparkles: add UserGreeting component to demonstrate conditional rendering

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,19 +1,20 @@
-import Header from './Header.jsx';
-import Footer from './Footer.jsx';
-import Card from './Card.jsx';
-import Button from './Button/Button.jsx';
-import Student from './Student.jsx';
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Card from "./Card.jsx";
+import Button from "./Button/Button.jsx";
+import Student from "./Student.jsx";
+import UserGreeting from "./UserGreeting.jsx";
 
 function App() {
-  
   return (
     <>
+      <UserGreeting isLoggedIn={true} />
       <Header />
       <Card />
-      <Student name='Spongebob' age={30} isStudent={true} />
-      <Student name='Patrick' age={42} isStudent={false} />
-      <Student name='Squidward' age={50} isStudent={false} />
-      <Student name='Sandy' age={27} isStudent={true} />
+      <Student name="Spongebob" age={30} isStudent={true} />
+      <Student name="Patrick" age={42} isStudent={false} />
+      <Student name="Squidward" age={50} isStudent={false} />
+      <Student name="Sandy" age={27} isStudent={true} />
       <Student />
       <Button />
       <Footer />
@@ -21,4 +22,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
