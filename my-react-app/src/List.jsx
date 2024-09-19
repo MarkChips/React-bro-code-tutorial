60c1e96cf3e31ed83ef805939b69c28cec043709
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-19 21:25:53 +0100

    feat: :sparkles: change fruits array items into objects, list different sort methods, assign ids and calories

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -1,11 +1,20 @@
 
 function List() {
 
-    const fruits = ['durian', 'banana', 'mango', 'strawberry', 'melon']
+    const fruits = [
+        { id: 1, name: 'durian', calories: 95 },
+        { id: 2, name: 'banana', calories: 45 },
+        { id: 3, name: 'mango', calories: 105 },
+        { id: 4, name: 'strawberry', calories: 159 },
+        { id: 5, name: 'melon', calories: 37 }
+    ];
 
-    fruits.sort();
+    // fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical
+    // fruits.sort((a, b) => b.name.localeCompare(a.name)); // Reverse Alphabetical
+    // fruits.sort((a, b) => a.calories - b.calories); // Numeric
+    fruits.sort((a, b) => b.calories - a.calories); // Reverse Numeric
 
-    const listItems = fruits.map(fruit => <li>{fruit}</li>);
+    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
 
     return (<ul>{listItems}</ul>);
 }
