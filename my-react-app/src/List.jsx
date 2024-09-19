f8668f16be23188f2d0ea61fc26ecdcbc829b5b5
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-19 21:32:16 +0100

    feat: :sparkles: create frilters for high and low calorie fruit

--- my-react-app/src/List.jsx
+++ my-react-app/src/List.jsx
@@ -12,9 +12,12 @@ function List() {
     // fruits.sort((a, b) => a.name.localeCompare(b.name)); // Alphabetical
     // fruits.sort((a, b) => b.name.localeCompare(a.name)); // Reverse Alphabetical
     // fruits.sort((a, b) => a.calories - b.calories); // Numeric
-    fruits.sort((a, b) => b.calories - a.calories); // Reverse Numeric
+    // fruits.sort((a, b) => b.calories - a.calories); // Reverse Numeric
 
-    const listItems = fruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
+    const lowCalFruits = fruits.filter(fruit => fruit.calories < 100);
+    const highCalFruits = fruits.filter(fruit => fruit.calories >= 100);
+
+    const listItems = lowCalFruits.map(fruit => <li key={fruit.id}>{fruit.name}: &nbsp; <b>{fruit.calories}</b></li>);
 
     return (<ul>{listItems}</ul>);
 }
