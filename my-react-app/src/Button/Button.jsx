cef184f659ca73c6814c50b7fb07e1e947909780
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-20 22:41:39 +0100

    feat: :sparkles: change text contents of button when double clicked

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -2,19 +2,10 @@ import styles from './Button.module.css';
 
 function Button() {
 
-  let count = 0;
-
-  const handleClick = (name) => {
-    if (count < 3) {
-      count++;
-      console.log(`${name} you clicked me ${count} time/s`)
-    } else {
-      console.log(`${name} stop clicking me!`)
-    }
-  }
+  const handleClick = (e) => e.target.textContent = "OUCH! 😫";
 
   return (
-    <button className={styles.button} onClick={() => handleClick('Mark')}>Click me 😁</button>
+    <button className={styles.button} onDoubleClick={(e) => handleClick(e)}>Click me 😁</button>
   );
 }
 
