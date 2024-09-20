5316a82ccf83c8db83c2ed5b4511428f2107ffb4
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-20 22:36:23 +0100

    feat: :sparkles: add conditional output to handleClick function

--- my-react-app/src/Button/Button.jsx
+++ my-react-app/src/Button/Button.jsx
@@ -2,12 +2,19 @@ import styles from './Button.module.css';
 
 function Button() {
 
-  const handleClick = () => console.log('OUCH!');
+  let count = 0;
 
-  const handleClick2 = (name) => console.log(`${name} stop clicking me`)
+  const handleClick = (name) => {
+    if (count < 3) {
+      count++;
+      console.log(`${name} you clicked me ${count} time/s`)
+    } else {
+      console.log(`${name} stop clicking me!`)
+    }
+  }
 
   return (
-    <button className={styles.button} onClick={() => handleClick2('Mark')}>Click me 😁</button>
+    <button className={styles.button} onClick={() => handleClick('Mark')}>Click me 😁</button>
   );
 }
 
