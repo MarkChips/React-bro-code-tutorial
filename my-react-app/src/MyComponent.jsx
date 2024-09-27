58805cae08ef0af271874bde2dd749e1910e3305
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-09-27 21:39:22 +0100

    feat: :sparkles: add example code for how to update the state of objects

--- my-react-app/src/MyComponent.jsx
+++ my-react-app/src/MyComponent.jsx
@@ -9,6 +9,7 @@ function MyComponent() {
     const [shipping, setShipping] = useState('Delivery');
     const [age, setAge] = useState(0);
     const [isEmployed, setIsEmployed] = useState(false);
+    const [car, setCar] = useState({ year: 2007, make: 'Mini', model: 'One' });
 
     function handleNameChange(event) {
         setName(event.target.value);
@@ -42,8 +43,26 @@ function MyComponent() {
         setIsEmployed(!isEmployed);
     }
 
+    function handleYearChange(event) {
+        setCar(car => ({ ...car, year: event.target.value }));
+    }
+
+    function handleMakeChange(event) {
+        setCar(c => ({ ...c, make: event.target.value }));
+    }
+
+    function handleModelChange(event) {
+        setCar(c => ({ ...c, model: event.target.value }));
+    }
+
     return (
         <div>
+            <p>Your favourite car is: {car.year} {car.make} {car.model}</p>
+
+            <input type="number" value={car.year} onChange={handleYearChange} /><br />
+            <input type="text" value={car.make} onChange={handleMakeChange} /><br />
+            <input type="text" value={car.model} onChange={handleModelChange} /><hr />
+
             <input value={name} onChange={handleNameChange} />
             <p>Name: {name}</p>
             <button onClick={updateName}>Set Name</button>
