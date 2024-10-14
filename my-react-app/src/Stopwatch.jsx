5cfdeb26a47ca6a26b6a19466e7ff029dc8b1915
Mark Chipperfield <mark-chipperfield@outlook.com>
2024-10-14 16:52:43 +0100

    style: :lipstick: add styling to Stopwatch component

--- my-react-app/src/Stopwatch.jsx
+++ my-react-app/src/Stopwatch.jsx
@@ -29,12 +29,15 @@ function Stopwatch() {
     }
 
     return (
-        <div className="stopwatch">
-            <div className="display">{formatTime()}</div>
-            <div className="controls">
-                <button onClick={start} className="start-button">Start</button>
-                <button onClick={stop} className="stop-button">Stop</button>
-                <button onClick={reset} className="reset-button">Reset</button>
+        <div className="stopwatch-container">
+            <div className="stopwatch">
+
+                <div className="display">{formatTime()}</div>
+                <div className="controls">
+                    <button onClick={start} className="start-button">Start</button>
+                    <button onClick={stop} className="stop-button">Stop</button>
+                    <button onClick={reset} className="reset-button">Reset</button>
+                </div>
             </div>
         </div>
     );
